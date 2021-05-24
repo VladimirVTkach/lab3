31128425748cf0a8ae0e0b666dedd652bcae0d04
Volodymyr <vladimirv.tkach@gmail.com>
2021-05-24 17:27:14 +0300

    Vertex class has been created

---
+++ src/main/java/Vertex.java
@@ -0,0 +1,26 @@
+import java.util.Objects;
+
+public class Vertex<T> {
+    private final T value;
+
+    public Vertex(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vertex<?> vertex = (Vertex<?>) o;
+        return Objects.equals(value, vertex.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
